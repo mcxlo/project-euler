b37f81837cdc4c51fa8d8a5fd3d1ffb8595de050
Seacell0 <macorrea-@hotmail.com>
2021-02-23 21:16:42 -0300

    mcxlo

---
+++ 01.multiplesof3and5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main()
+{
+ 
+ int result;
+
+for(int i = 1; i < 1000; i++)
+    if (i % 3 == 0 || i % 5 == 0 )
+        result = result + i;
+        printf("%d\n", result);
+    
+
+}
